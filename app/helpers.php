49928dff1060c2959572cdf483e11db2abc74683
nhutmd <nhutminhdoan.com@gmail.com>
2018-09-23 22:50:53 +0700

    add STATUS_ORDER, genCodeOrder function

--- app/helpers.php
+++ app/helpers.php
@@ -16,11 +16,26 @@ class helpers
     public static $ROLE = ['ADMIN' => 0, 'USER' => 1];
     public static $USER_SEX = ['MALE' => 1, 'FEMALE' => 0];
     public static $STATUS_USER = ['BLOCK' => 0, 'ACTIVE' => 1];
+    public static $STATUS_ORDER = [
+        'CANCEL' => -1,
+        'INIT' => 0,
+        'UPDATED' => 1,
+        'BOOK' => 2,
+        'QUEUED' => 3,
+        'LOCKED' => 4,
+        'TRACKING' => 5,
+        'FINISHED' => 6
+    ];
     public static $COINS_INIT = 0;
     public static $COIN_PAY = ['RECHARGE' => 0, 'FEES' => 1, 'WAGE' => 2];
 
     public static function isActive($name)
     {
         return Route::current()->getName() == $name ? 'class=active' : '';
+    }
+
+    public static function genCodeOrder()
+    {
+        return 'TT' . date('y') . date('m') . date('d') . date('H') . date('i') . date('s');
     }
 }
